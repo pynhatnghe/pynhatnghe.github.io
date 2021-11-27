36c1e1cf89d66ad9e7f5ac5aeb94a6721976e45f
Hien Luong <hyhien@gmail.com>
2021-11-27 20:01:28 +0700

    Da hinh

--- python11112021/buoi07_27.11_oop/KeThua.py
+++ python11112021/buoi07_27.11_oop/KeThua.py
@@ -1,3 +1,6 @@
+import math
+
+
 class HinhHoc:
     DienTich = 0
     ChuVi = 0
@@ -25,8 +28,25 @@ class HinhChuNhat(HinhHoc):
         return "D = " + str(self.Dai) + ", R = " + str(self.Rong) + ",S = " + str(self.DienTich) + ", P = " + str(self.ChuVi)
 
 
-a = HinhHoc()
-print(a)
-b = HinhChuNhat(7, 9)
-b.tinh_dientich_chuvi()
-print(b)
+class HinhTron(HinhHoc):
+    BanKinh = 0
+
+    def __init__(self, r):
+        self.BanKinh = r
+
+    def tinh_dientich_chuvi(self):
+        self.DienTich = math.pi * (self.BanKinh ** 2)
+        self.ChuVi = math.pi * self.BanKinh * 2
+
+    def __str__(self):
+        return "R = " + str(self.BanKinh) + ", S = " + str(self.DienTich) + ", P = " + str(self.ChuVi)
+
+
+mang_hinh = []
+mang_hinh.append(HinhChuNhat(34, 56))
+mang_hinh.append(HinhChuNhat(13, 5))
+mang_hinh.append(HinhTron(23))
+
+for item in mang_hinh:
+    item.tinh_dientich_chuvi()
+    print(item)
