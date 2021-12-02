7a06f0f18758808a0f38194023e824cad81b70dc
Hien Luong <hyhien@gmail.com>
2021-12-02 21:01:14 +0700

    Xu ly su kien

--- python11112021/buoi09_02.12_tkinter/Calculator.py
+++ python11112021/buoi09_02.12_tkinter/Calculator.py
@@ -1,8 +1,38 @@
 from tkinter import *
+
 root = Tk()
 root.title("Calculator")
 root.geometry("300x345")
-root.resizable(0, 0)  # Cam resize
+root.resizable(0, 0)  # Cannot resize
+
+
+def click_number(item):
+    global expression
+    expression += str(item)
+    input_text.set(expression)
+
+
+def click_clear():
+    global expression
+    expression = ""
+    input_text.set("")
+
+
+def click_back():
+    global expression
+    expression = expression[:-1]
+    input_text.set(expression)
+
+
+def xu_ly_cham():
+    global expression
+    if '.' not in expression:
+        if len(expression) == 0:
+            expression += "0."
+        else:
+            expression += "."
+    input_text.set(expression)
+
 
 expression = ""
 input_text = StringVar()
@@ -21,46 +51,46 @@ input_field.pack(ipady=10)  # internal padding y
 button_frame = Frame(root, width=300, height=275, bd=0,
                      highlightbackground="black", highlightcolor="black", highlightthickness=2)
 button_frame.pack(side=TOP)
-clear = Button(button_frame, text="Clear", height=3, width=20).grid(
+clear = Button(button_frame, text="Clear", height=3, width=20, command=lambda: click_clear()).grid(
     row=0, column=0, columnspan=2, padx=1, pady=1)
-back = Button(button_frame, text="<--", height=3, width=20).grid(
+back = Button(button_frame, text="<--", height=3, width=20, command=lambda: click_back()).grid(
     row=0, column=2, columnspan=2, padx=1, pady=1)
 
 # Row 1: 7,8,9, /
-so7 = Button(button_frame, text="7", width=9, height=3).grid(
+so7 = Button(button_frame, text="7", width=9, height=3, command=lambda: click_number(7)).grid(
     row=1, column=0, padx=1, pady=1)
-so8 = Button(button_frame, text="8", width=9, height=3).grid(
+so8 = Button(button_frame, text="8", width=9, height=3, command=lambda: click_number(8)).grid(
     row=1, column=1, padx=1, pady=1)
-so8 = Button(button_frame, text="9", width=9, height=3).grid(
+so8 = Button(button_frame, text="9", width=9, height=3, command=lambda: click_number(9)).grid(
     row=1, column=2, padx=1, pady=1)
 chia = Button(button_frame, text="/", width=9, height=3).grid(
     row=1, column=3, padx=1, pady=1)
 
 # Row 2: 4, 5, 6, *
-so4 = Button(button_frame, text="4", width=9, height=3).grid(
+so4 = Button(button_frame, text="4", width=9, height=3, command=lambda: click_number(4)).grid(
     row=2, column=0, padx=1, pady=1)
-so5 = Button(button_frame, text="5", width=9, height=3).grid(
+so5 = Button(button_frame, text="5", width=9, height=3, command=lambda: click_number(5)).grid(
     row=2, column=1, padx=1, pady=1)
-so6 = Button(button_frame, text="6", width=9, height=3).grid(
+so6 = Button(button_frame, text="6", width=9, height=3, command=lambda: click_number(6)).grid(
     row=2, column=2, padx=1, pady=1)
 nhan = Button(button_frame, text="*", width=9, height=3).grid(
     row=2, column=3, padx=1, pady=1)
 
 # Row 3: 1, 2, 3, -
-so1 = Button(button_frame, text="1", width=9, height=3).grid(
+so1 = Button(button_frame, text="1", width=9, height=3, command=lambda: click_number(1)).grid(
     row=3, column=0, padx=1, pady=1)
-so2 = Button(button_frame, text="2", width=9, height=3).grid(
+so2 = Button(button_frame, text="2", width=9, height=3, command=lambda: click_number(2)).grid(
     row=3, column=1, padx=1, pady=1)
-so3 = Button(button_frame, text="3", width=9, height=3).grid(
+so3 = Button(button_frame, text="3", width=9, height=3, command=lambda: click_number(3)).grid(
     row=3, column=2, padx=1, pady=1)
 tru = Button(button_frame, text="-", width=9, height=3).grid(
     row=3, column=3, padx=1, pady=1)
 
 
 # Row 4: 0, ., =, +
-so0 = Button(button_frame, text="0", width=9, height=3).grid(
+so0 = Button(button_frame, text="0", width=9, height=3, command=lambda: click_number(0)).grid(
     row=4, column=0, padx=1, pady=1)
-cham = Button(button_frame, text=".", width=9, height=3).grid(
+cham = Button(button_frame, text=".", width=9, height=3, command=lambda: xu_ly_cham()).grid(
     row=4, column=1, padx=1, pady=1)
 bang = Button(button_frame, text="=", width=9, height=3).grid(
     row=4, column=2, padx=1, pady=1)
