77cd5b58e93e062c4a1912f62220a0349da2c0ae
Hien Luong <hyhien@gmail.com>
2021-12-02 20:00:27 +0700

    Complete Login

--- python11112021/buoi09_02.12_tkinter/Login.py
+++ python11112021/buoi09_02.12_tkinter/Login.py
@@ -1,16 +1,20 @@
 from tkinter import *
+from tkinter import messagebox
 from functools import partial
 
 
-def xu_ly_login(username, password):
+def xu_ly_login(username, password, remember_me):
     print("Username", username.get())
     print("Password", password.get())
+    messagebox.showinfo("Login", "Username: " + username.get() +
+                        "\nPassword: " + password.get() +
+                        "\nRemember: " + str(remember_me.get()))
 
 
 root = Tk()
 
 root.title("Login form")
-root.geometry("250x100")
+root.geometry("250x123")
 
 lblTitle = Label(root, text="LOGIN",
                  font="Arial 12 bold").grid(row=0, column=1)
@@ -21,9 +25,12 @@ password = StringVar()
 txtUser = Entry(root, textvariable=username, width=27).grid(row=1, column=1)
 txtPass = Entry(root, textvariable=password, width=27).grid(row=2, column=1)
 
-process_login = partial(xu_ly_login, username, password)
+remember_me = IntVar()
+Checkbutton(root, text="Ghi nhớ", variable=remember_me).grid(row=4, sticky=W)
+process_login = partial(xu_ly_login, username, password, remember_me)
 btnLogin = Button(root, text="Login",
                   command=process_login).grid(row=3, column=1)
+
 
 # Run
 root.mainloop()
