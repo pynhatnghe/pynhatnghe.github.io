be7f14016059718d35fa63d9073c2afc69b255df
Hien Luong <hyhien@gmail.com>
2021-12-04 19:43:03 +0700

    Provide QuanLyHangHoa

---
+++ python11112021/buoi10_04.12_tkinter/QuanLyHangHoa.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from dbcommon import MyDatabase
+
+root = Tk()
+# Config
+root.geometry("450x300")
+root.resizable(False, False)
+root.title("Quản lý hàng hóa")
+
+# Menu
+menuBar = Menu(root)
+mnuLoai = Menu(menuBar, tearoff=0)
+mnuLoai.add_command(label="Thêm loại")
+mnuLoai.add_command(label="Quit", command=root.destroy)
+menuBar.add_cascade(label="Quản lý loại", menu=mnuLoai)
+mnuHangHoa = Menu(menuBar, tearoff=0)
+mnuHangHoa.add_command(label="Hàng hóa")
+menuBar.add_cascade(label="Quản lý Hàng hóa", menu=mnuHangHoa)
+
+root.config(menu=menuBar)
+
+
+# Main
+root.mainloop()
+
+# Create loai, hanghoa
+
+
+def create_loai(connect):
+    sql = '''
+    CREATE TABLE Loai(
+        MaLoai integer PRIMARY KEY AUTOINCREMENT,
+        TenLoai varchar(40)
+    )
+    '''
+    MyDatabase.create_table(connect, sql)
+
+
+def create_hanghoa(connect):
+    sql = '''
+    CREATE TABLE HangHoa(
+        MaHH char(6) PRIMARY KEY,
+        TenHH varchar(40),
+        MoTa varchar(55),
+        DonGia decimal(10,2),
+        SKU varchar(15) NULL,
+        MaLoai interger NULL,
+        FOREIGN KEY (MaLoai) REFERENCES Loai(MaLoai)
+    )
+    '''
+    MyDatabase.create_table(connect, sql)
+
+
+myconnection = MyDatabase.sql_connection()
+if myconnection:
+    create_loai(myconnection)
+    create_hanghoa(myconnection)
---
+++ python11112021/buoi10_04.12_tkinter/dbcommon.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+class MyDatabase:
+    @staticmethod
+    def sql_connection():
+        try:
+            return sqlite3.connect("MyDatabase.db")
+        except Exception as ex:
+            print(ex)
+            return None
+
+    @staticmethod
+    def run_statement(connection, sql_statetement):
+        try:
+            cursor = connection.cursor()
+            cursor.execute(sql_statetement)
+            connection.commit()
+        except Exception as ex:
+            print(ex)
+
+    @staticmethod
+    def insert_and_get_lasted_id(connection, sql_statetement):
+        try:
+            cursor = connection.cursor()
+            cursor.execute(sql_statetement)
+            connection.commit()
+            return cursor.lastrowid
+        except Exception as ex:
+            print(ex)
+            return None
+
+    @staticmethod
+    def query_data(connection, sql_statetement):
+        try:
+            cursor = connection.cursor()
+            cursor.execute(sql_statetement)
+            return cursor.fetch_all()
+        except Exception as ex:
+            print(ex)
+            return None
