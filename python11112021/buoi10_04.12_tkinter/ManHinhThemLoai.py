b4d02dc99a3a797ff6777b3cd7b83659687d73a8
Hien Luong <hyhien@gmail.com>
2021-12-04 20:24:36 +0700

    update ManHinhLoai

--- python11112021/buoi10_04.12_tkinter/ManHinhThemLoai.py
+++ python11112021/buoi10_04.12_tkinter/ManHinhThemLoai.py
@@ -1,8 +1,15 @@
 from tkinter import *
+from tkinter import font
 
 
 def openManHinhThemLoai(root):
     mhLoai = Toplevel(root)
-    mhLoai.geometry("450x300")
+    mhLoai.geometry("300x100")
     mhLoai.resizable(False, False)
     mhLoai.title("Màn hình Thêm loại")
+    Label(mhLoai, text="Quản lý Thêm loại",
+          font="Arial 13 bold").grid(row=0, columnspan=3)
+    Label(mhLoai, text="Tên loại").grid(row=1, column=0)
+    tenLoai = StringVar()
+    Entry(mhLoai, textvariable=tenLoai).grid(row=1, column=1)
+    Button(mhLoai, text="Thêm").grid(row=1, column=2)
