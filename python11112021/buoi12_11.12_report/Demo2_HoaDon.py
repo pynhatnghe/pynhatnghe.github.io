7835a56cea687f10c2302d0d93ee39c034c916e5
Hien Luong <hyhien@gmail.com>
2021-12-11 19:40:51 +0700

    Comple output HoaDon

--- python11112021/buoi12_11.12_report/Demo2_HoaDon.py
+++ python11112021/buoi12_11.12_report/Demo2_HoaDon.py
@@ -8,8 +8,11 @@ order_template = '''
 - Khách hàng: {{ data.ho_ten }}
 - Số điện thoại: {{ data.dien_thoai }}
     DANH SÁCH HÀNG MUA
-    <số lượng> tên x giá = thành tiền
-        Tổng tiền: 
+{# Ghi chú vùng hiển thị thông tin hàng mua #}
+{% for item in data.hang_hoa %}
+    {{ item.so_luong }} x {{ item.ten }} - {{ item.gia }} đ = {{ item.so_luong * item.gia}} đ.
+{% endfor%}
+        Tổng tiền: {{ data.tong_tien }} đ.
     Cảm ơn quý khách đã tin dùng dịch vụ của chúng tôi.
 '''
 data_hoadon = {
@@ -21,5 +24,7 @@ data_hoadon = {
         {"ten": "Máy lọc không khí Daikin", "so_luong": 1, "gia": 4299}
     ]
 }
+data_hoadon["tong_tien"] = (sum(item["so_luong"] * item["gia"]
+                            for item in data_hoadon["hang_hoa"]))
 hoadon_template = Template(order_template)
 print(hoadon_template.render(data=data_hoadon))
