91384051119a2c6dd0aa3fa6f5212a5912c5d113
Hien Luong <hyhien@gmail.com>
2021-12-14 19:57:28 +0700

    Update process json

--- python11112021/buoi13_14.12_csv_excel/2.DocFileCSV.py
+++ python11112021/buoi13_14.12_csv_excel/2.DocFileCSV.py
@@ -1,6 +1,7 @@
 # Data Quận huyện lấy từ https://www.gso.gov.vn/phuong-phap-thong-ke/danh-muc/don-vi-hanh-chinh/
 
 import csv
+import json
 with open("data/DSTinhKemQuanHuyen_14_12_2021.csv", encoding="utf-8") as file:
     rows = csv.reader(file)
     for row_obj in rows:
@@ -9,9 +10,27 @@ with open("data/DSTinhKemQuanHuyen_14_12_2021.csv", encoding="utf-8") as file:
 
 
 # ========VD2: Đọc lên dạng Dict
+# result = {
+#     "Tỉnh Cà Mau": {
+#         "SoXaPhuong": 7
+#     }
+# }
 with open("data/DSTinhKemQuanHuyen_14_12_2021.csv", encoding="utf-8") as file:
     my_dicts = csv.DictReader(file)
     print(type(my_dicts))
+
+    result = {}  # Chứa danh sách thống kê xã phường theo tỉnh thành
+
     for item in my_dicts:
-        # print(item)
+        print(item)
         print(item['\ufeffTỉnh Thành Phố'], item['Quận Huyện'])
+        tinh = item['\ufeffTỉnh Thành Phố']
+        if tinh in result:
+            result[tinh]["SoXaPhuong"] += 1  # tăng 1 xã
+        else:
+            result[tinh] = {
+                "SoXaPhuong": 1
+            }
+
+    json_str = json.dumps(result, indent=4, ensure_ascii=False)
+    print(json_str)
