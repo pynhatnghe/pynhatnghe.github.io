c221a1684fdcf8937d25668294c795d48a0f21e4
Hien Luong <hyhien@gmail.com>
2021-12-16 19:49:22 +0700

    API Read data json

--- python11112021/buoi14_16.12_FastAPI/main.py
+++ python11112021/buoi14_16.12_FastAPI/main.py
@@ -1,4 +1,9 @@
+# pip install fastapi
+# pip install uvicorn
+
 from fastapi import FastAPI
+import os
+import json
 
 app = FastAPI()
 
@@ -12,3 +17,20 @@ def api_root():
 @app.get("/hello/{name}")  # host/hello/Admin
 def say_hello(name: str):
     return f"Hello {name}"
+
+
+@app.get("/khachhang/{maso}")
+def read_data_customer(maso: int):
+    # Đọc file json tương ứng với maso và trả về
+    filename = f"data/kh{str(maso).zfill(3)}.json"
+    if os.path.exists(filename):  # Nếu có
+        with open(filename, "r") as myfile:
+            return {
+                "success": True,
+                "data": myfile.read()
+            }
+    else:
+        return {
+            "success": False,
+            "message": "Customer not found"
+        }
