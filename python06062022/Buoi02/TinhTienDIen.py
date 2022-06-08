4d43c57c64e20820f6f4a2ce34336ce870fb9f29
hyhien <hyhien@gmail.com>
2022-06-08 18:48:55 +0700

    Yeu cau Tinh tien Dien

---
+++ python06062022/Buoi02/TinhTienDIen.py
@@ -0,0 +1,10 @@
+'''
+Giá bán lẻ điện sinh hoạt
+Bậc 1: Cho kWh từ 0 - 50        1.678
+Bậc 2: Cho kWh từ 51 - 100      1.734
+Bậc 3: Cho kWh từ 101 - 200     2.014
+Bậc 4: Cho kWh từ 201 - 300     2.536
+Bậc 5: Cho kWh từ 301 - 400     2.834
+Bậc 6: Cho kWh từ 401 trở lên   2.927
+Yêu cầu: Nhập chỉ số cũ, chỉ số mới & tính tiền
+'''
