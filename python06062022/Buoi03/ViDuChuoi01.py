b3410b7f348388698dd2f759f670ed5df6ab63af
hyhien <hyhien@gmail.com>
2022-06-10 19:02:15 +0700

    Vi du dau tien ve vi tri

---
+++ python06062022/Buoi03/ViDuChuoi01.py
@@ -0,0 +1,14 @@
+"""Các hàm xử lý chuỗi"""
+'''
+Một số phím tắt của PyCharm
+Ctrl + / : bật/bỏ ghi chú
+Ctrl + D : duplicate (nhân đôi) khối (nếu có chọn)/dòng
+Alt + Enter: nhắc lệnh  
+'''
+# chuoi = input("Nhập chuỗi: ")
+chuoi = "Lop python Nhất Nghệ"
+print(chuoi[4:10]) # từ vị trí 4 đến vị trí 10
+print(chuoi[5:])
+print(chuoi[:11]) # từ đầu đến vị trí 10
+print(chuoi[-4:]) # từ vị trí -4 đến cuối
+print(chuoi[:-9]) # từ đầu đến vị trí -10
