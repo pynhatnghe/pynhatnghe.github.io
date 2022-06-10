086e6d0f60bb0012e122fde67f8f9a9ac1c96687
hyhien <hyhien@gmail.com>
2022-06-10 18:33:51 +0700

    push all

---
+++ python06062022/Buoi01/main.py
@@ -0,0 +1,9 @@
+'''Ghi chú chơi cho biết'''
+# Noted Python
+"""
+Ghi chú nhiều dòng
+"""
+print("Nhất Nghệ Python")
+print('Khóa Python 06.06')
+ho_ten = input("Nhập họ tên:")
+print("Xin chào: " + ho_ten)
