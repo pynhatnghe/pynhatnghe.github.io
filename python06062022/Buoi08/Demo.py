c42dbfe1348a67af73475f1635f878c4d62454b1
hyhien <hyhien@gmail.com>
2022-06-22 21:19:39 +0700

    Demo

--- python06062022/Buoi08/Demo.py
+++ python06062022/Buoi08/Demo.py
@@ -5,6 +5,14 @@ from DatabaseUtils import DB_FILE, SqliteUtil
 # print(DB_FILE)
 # print(SqliteUtil.create_connection())
 
+def insert_loai(ma_loai, ten_loai):
+    sql_insert_loai = f'''
+    INSERT INTO Loai(MaLoai, TenLoai)
+    VALUES('{ma_loai}', '{ten_loai}')
+    '''
+    if SqliteUtil.execute(sql_insert_loai):
+        print('Thêm loại thành công')
+
 if __name__ == '__main__':
     # Tạo 2 table
     sql_create_table_loai = """
@@ -26,3 +34,8 @@ if __name__ == '__main__':
      """
     SqliteUtil.execute(sql_create_table_loai)
     SqliteUtil.execute(sql_create_table_hang_hoa)
+
+    # Thêm loại
+    insert_loai(1, 'Bánh mì')
+    insert_loai(2, 'Bánh canh')
+
