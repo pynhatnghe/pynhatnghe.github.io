d796fa9afcd74f005f7c93cefffc35d5432e0af9
hyhien <hyhien@gmail.com>
2022-06-29 20:33:48 +0700

    Design LOGIN

--- python06062022/Buoi09/LoginGUI.py
+++ python06062022/Buoi09/LoginGUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 root = Tk()
 root.title("LOGIN - Authenticate user")
@@ -11,9 +12,25 @@ ttk.Label(root, text="Username")\
 ttk.Label(root, text="Password")\
     .grid(column=0, row=1, padx=5, pady=5)
 
-ttk.Entry(root).grid(column=1, row=0)
-ttk.Entry(root).grid(column=1, row=1)
+# Đặt 2 biến để giữ giá trị 2 entry
+username = StringVar(value="user")
+password = StringVar()
+ttk.Entry(root, textvariable=username)\
+    .grid(column=1, row=0)
+ttk.Entry(root, textvariable=password, show="*")\
+    .grid(column=1, row=1)
 
-ttk.Button(root, text="LOGIN").grid(column=1, row=2)
+def handle_login(user, passw):
+    messagebox.showinfo("INFO",
+    f"Bạn nhập:\nUsername:{user}\nPassword: {passw}")
+
+from functools import partial
+process_login = partial(
+    handle_login,
+    user=username.get(),
+    passw=password.get()
+)
+ttk.Button(root, text="LOGIN", command=process_login)\
+    .grid(column=1, row=2, sticky=E)
 
 root.mainloop()
