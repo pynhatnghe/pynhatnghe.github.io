cb6b0aab1db9ac6786d60202a29a509130315814
hyhien <hyhien@gmail.com>
2022-06-29 19:24:04 +0700

    Tkinter show image

--- python06062022/Buoi09/ViDu01.py
+++ python06062022/Buoi09/ViDu01.py
@@ -12,5 +12,10 @@ Label(root,
 ).pack()
 Label(root, text= "Khai giảng: 06/06/2022").pack()
 
+# Chú ý file dạng PNG/GIF
+# img = PhotoImage(file="Python-Logo.png")
+img = PhotoImage(file="python_gif.gif")
+Label(root, image=img).pack()
+
 # Hiện cửa sổ chính
 root.mainloop()
