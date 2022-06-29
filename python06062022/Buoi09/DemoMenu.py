f5fd5c6073924058e1478031bd09ed650a4e0116
hyhien <hyhien@gmail.com>
2022-06-29 21:12:35 +0700

    Demo menu 3

--- python06062022/Buoi09/DemoMenu.py
+++ python06062022/Buoi09/DemoMenu.py
@@ -15,7 +15,7 @@ file_menu.add_command(label='Exit',command=root.destroy)
 menu_bar.add_cascade(label="File",menu=file_menu)
 
 mnu_tool = Menu(menu_bar, tearoff=0)
-mnu_tool.add_command("About")
+mnu_tool.add_command(label="About")
 menu_bar.add_cascade(label="Tools",menu=mnu_tool)
 ###############/End Thêm menu item + xử lý*************
 
