e37f5c427c7a075d667fb703bb1ec018710eb2e3
hyhien <hyhien@gmail.com>
2022-06-29 21:00:37 +0700

    Demo menu1

---
+++ python06062022/Buoi09/DemoMenu.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+# Khai báo menu bar
+menu_bar = Menu(root)
+
+###############Thêm menu item + xử lý*************
+###############/End Thêm menu item + xử lý*************
+
+# Nhúng (add) menubnar vào màn hình chính
+root.config(menu=menu_bar)
+
+root.mainloop()
