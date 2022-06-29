01d937786311901b3c361eb956a607acecc1b46a
hyhien <hyhien@gmail.com>
2022-06-29 21:12:02 +0700

    Demo menu 3

--- python06062022/Buoi09/DemoMenu.py
+++ python06062022/Buoi09/DemoMenu.py
@@ -6,12 +6,17 @@ root = Tk()
 menu_bar = Menu(root)
 
 ###############Thêm menu item + xử lý*************
-file_menu = Menu(menu_bar)
+file_menu = Menu(menu_bar, tearoff=False)
 file_menu.add_command(label='Open')
 file_menu.add_command(label='Save')
+file_menu.add_separator()
 file_menu.add_command(label='Exit',command=root.destroy)
 
 menu_bar.add_cascade(label="File",menu=file_menu)
+
+mnu_tool = Menu(menu_bar, tearoff=0)
+mnu_tool.add_command("About")
+menu_bar.add_cascade(label="Tools",menu=mnu_tool)
 ###############/End Thêm menu item + xử lý*************
 
 # Nhúng (add) menubnar vào màn hình chính
