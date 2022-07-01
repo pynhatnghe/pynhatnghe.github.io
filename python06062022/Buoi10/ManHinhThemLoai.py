8df2ea559aef75dc28efd67ab9639bcd29dc3960
hyhien <hyhien@gmail.com>
2022-07-01 21:14:56 +0700

    Xu ly them loai

--- python06062022/Buoi10/ManHinhThemLoai.py
+++ python06062022/Buoi10/ManHinhThemLoai.py
@@ -4,16 +4,24 @@ from tkinter import messagebox
 
 def mo_man_hinh_them_loai(root):
     def them_moi_loai():
-        sql = f'''
-        INSERT INTO Loai(TenLoai) VALUES('{ten_loai.get()}')
+        sql_select = f'''
+        SELECT * FROM Loai WHERE TenLoai ='{ten_loai.get()}' 
         '''
-        print(sql)
-        ma_loai = SqliteUtil.insert_and_get_lasted_id(sql)
-        if ma_loai:
-            messagebox.showinfo(
-                "Thông báo",
-            f"Thêm thành công loại {ten_loai.get()} mã {ma_loai}"
-            )
+        data_return = SqliteUtil.query(sql_select)
+
+        if len(data_return) > 0:
+            messagebox.showwarning("Cảnh báo", "Đã tồn tại")
+        else:
+            sql = f'''
+            INSERT INTO Loai(TenLoai) VALUES('{ten_loai.get()}')
+            '''
+            print(sql)
+            ma_loai = SqliteUtil.insert_and_get_lasted_id(sql)
+            if ma_loai:
+                messagebox.showinfo(
+                    "Thông báo",
+                f"Thêm thành công loại {ten_loai.get()} mã {ma_loai}"
+                )
 
     # Khai báo màn hình con của root
     mh_loai = Toplevel(root)
