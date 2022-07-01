3940eba2dd97f1cebc805bace4f8c605d16c8042
hyhien <hyhien@gmail.com>
2022-07-01 21:10:30 +0700

    Xu ly them loai

--- python06062022/Buoi10/ManHinhThemLoai.py
+++ python06062022/Buoi10/ManHinhThemLoai.py
@@ -1,6 +1,20 @@
 from tkinter import *
+from DatabaseUtils import SqliteUtil
+from tkinter import messagebox
 
 def mo_man_hinh_them_loai(root):
+    def them_moi_loai():
+        sql = f'''
+        INSERT INTO Loai(TenLoai) VALUES('{ten_loai.get()}')
+        '''
+        print(sql)
+        ma_loai = SqliteUtil.insert_and_get_lasted_id(sql)
+        if ma_loai:
+            messagebox.showinfo(
+                "Thông báo",
+            f"Thêm thành công loại {ten_loai.get()} mã {ma_loai}"
+            )
+
     # Khai báo màn hình con của root
     mh_loai = Toplevel(root)
     mh_loai.title("Thêm loại")
@@ -12,7 +26,7 @@ def mo_man_hinh_them_loai(root):
         textvariable=ten_loai
     ).grid(column=1,row=0)
     Button(
-        mh_loai, text="Thêm mới"
+        mh_loai, text="Thêm mới", command=them_moi_loai
     ).grid(column=1,row=1,sticky=E)
 
     mh_loai.mainloop()
