89aa8754715de590cab4811cf62d2258efe2016c
hyhien <hyhien@gmail.com>
2022-07-20 21:11:38 +0700

    Restful API PUT, DELETE

--- python06062022/Buoi17/server/api.py
+++ python06062022/Buoi17/server/api.py
@@ -45,11 +45,31 @@ def insert_loai(model: Loai):
     new_loai_id = MySqlUtil.insert_and_get_lasted_id(sql)
     if new_loai_id:
         return {
-            "succes": True,
-            "data": MySqlUtil.query(new_loai_id)
+            "success": True,
+            "data": MySqlUtil.query(
+                f"SELECT * FROM loai WHERE MaLoai = {new_loai_id}"
+            )
         }
     else:
         return {
             "success": False,
             "message": "Thêm loại không thành công"
         }
+
+
+@app.delete("/loai/{ma_loai}", tags=["Category"])
+def delete_loai(ma_loai: int):
+    sql = f"DELETE FROM loai WHERE MaLoai = {ma_loai}"
+    if MySqlUtil.execute(sql):
+        return {"success": True}
+    return {"success": True}
+
+@app.put("/loai/{ma_loai}", tags=["Category"])
+def update_loai(ma_loai: int, model: Loai):
+    sql = f'''
+    UPDATE loai SET TenLoai = '{model.TenLoai}', Hinh = '{model.Hinh}' 
+    WHERE MaLoai = {ma_loai}
+'''
+    if MySqlUtil.execute(sql):
+        return {"success": True}
+    return {"success": True}
