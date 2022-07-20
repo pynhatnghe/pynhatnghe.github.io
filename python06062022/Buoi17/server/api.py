9bf46a780b33061529192435faae9cde9ae0209a
hyhien <hyhien@gmail.com>
2022-07-20 20:36:29 +0700

    Restful GET

--- python06062022/Buoi17/server/api.py
+++ python06062022/Buoi17/server/api.py
@@ -13,3 +13,20 @@ def get_all_loai():
     data = MySqlUtil.query("SELECT * FROM loai")
     print(data)
     return data
+
+
+@app.get("/loai/{ma_loai}", tags=["Category"])
+def get_loai(ma_loai: int):
+    sql = f"SELECT * FROM loai WHERE MaLoai = {ma_loai}"
+    data = MySqlUtil.query(sql)
+    if len(data) == 0:
+        return None
+    return data[0]
+
+@app.get("/loai/search/by", tags=["Category"])
+def search_loai(keyword: str):
+    sql = f"SELECT * FROM loai WHERE TenLoai LIKE '%{keyword}%'"
+    print(sql)
+    data = MySqlUtil.query(sql)
+    print(data)
+    return data
