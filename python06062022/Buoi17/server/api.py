574704519e07e64fdf134ec3f91d53e0f0a961ea
hyhien <hyhien@gmail.com>
2022-07-20 20:01:03 +0700

    Get All category

--- python06062022/Buoi17/server/api.py
+++ python06062022/Buoi17/server/api.py
@@ -1,8 +1,15 @@
 # File api.py
 from fastapi import FastAPI
+from .database_util import MySqlUtil
 
 app = FastAPI()
 
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
+
+@app.get("/loai", tags=["Category"])
+def get_all_loai():
+    data = MySqlUtil.query("SELECT * FROM loai")
+    print(data)
+    return data
